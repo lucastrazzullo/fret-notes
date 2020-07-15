28a40994fe01838018a0bdb1a72751df78d9d8ae
Luca Strazzullo <lucastrazzullo86@gmail.com>
2020-07-15 16:38:56 +0200

    Created Project

---
+++ Fret Notes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fret Notes
+//
+//  Created by luca strazzullo on 15/7/20.
+//  Copyright © 2020 Luca Strazzullo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
